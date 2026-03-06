cc59fea001a994d4539bab105b543c32552d0dfd
sagarsolanki139277-ai <sagar.solanki139277@marwadiuniversity.ac.in>
2026-03-06 11:08:07 +0530

    Delete unit 2 directory

--- unit 2/program 6.py
+++
@@ -1,5 +0,0 @@
-with open("numbers.txt", "r") as f:
-    numbers = list(map(int, f.read().split()))
-    print("Total:", sum(numbers))
-    print("Max:", max(numbers))
-    print("Min:", min(numbers))
