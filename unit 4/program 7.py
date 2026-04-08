7ac6b8c15e08bc5295a7c790ffa7a807648cbddb
sagarsolanki139277-ai <sagar.solanki139277@marwadiuniversity.ac.in>
2026-04-08 11:54:11 +0530

    Update program 7.py

--- unit 4/program 7.py
+++ unit 4/program 7.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 df = pd.read_excel("students.xlsx")
 gender_counts = df['Gender'].value_counts()
 
-plt.bar(gender_counts.index, gender_counts.values, color=['blue', 'pink'])
+plt.bar(gender_counts.index, gender_counts.values, color=['blue', 'black'])
 plt.title("Gender Distribution")
 plt.xlabel("Gender")
 plt.ylabel("Number of Students")
