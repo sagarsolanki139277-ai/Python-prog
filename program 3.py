008032e8630c0220c97df5eeb38061fc1bfe3f74
sagarsolanki139277-ai <sagar.solanki139277@marwadiuniversity.ac.in>
2026-04-08 11:50:28 +0530

    Delete program 3.py

--- program 3.py
+++
@@ -1,9 +0,0 @@
-# Write a program to load above excel file and use dropna() and fillna() functions separately.
-import pandas as pd
-df = pd.read_excel("students.xlsx")
-
-print("\nUsing dropna():")
-print(df.dropna())
-
-print("\nUsing fillna():")
-print(df.fillna("Not Available"))
